f9702da117f18f8a211d53178018bf300ae6e299
Błażej Wróbel <blazej.wrobel@microsoft.wsei.edu.pl>
2020-06-08 12:20:54 +0200

    working MainMenu

--- DCN/DCN.py
+++ DCN/DCN.py
@@ -1,1 +1,54 @@
+import repository
+import os
 
+while True:
+    try:
+        os.system('cls')    # For Windows
+    except:
+        continue
+    try:
+        os.system('clear')  # For Linux/OS X
+    except:
+        continue
+    repository.GlobalMenu()
+    try:
+        option = int(input())
+        if option == 0:
+            print("Zamykanie Menu")
+            break
+        elif option == 1:
+            try:
+                repository.Install()
+            except:
+                repository.Error("Install")
+                os.system("PAUSE")
+                continue
+        elif option == 2:
+            try:
+                repository.StartUpdate()
+            except:
+                repository.Error("StartUpdate")
+                os.system("PAUSE")
+                continue
+        elif option == 3:
+            try:
+                repository.StopUpdate()
+            except:
+                repository.Error("StopUpdate")
+                os.system("PAUSE")
+        elif option == 4:
+            try:
+                repository.Update()
+            except:
+                repository.Error("Update")
+                os.system("PAUSE")
+        elif option == 5:
+            try:
+                repository.Statistics()
+            except:
+                repository.Error("Statistics")
+                os.system("PAUSE")
+    except:
+        print("Nie wybrano żadnej z dostępnych opcji, spróbuj jeszcze raz")
+        os.system("PAUSE")
+        continue
---
+++ DCN/repository.py
@@ -0,0 +1,13 @@
+
+def GlobalMenu():
+    options = ["Instalacja aplikacji","Start Update","Stop Update","Aktualizacja","Statystyki","Wyjście"]
+    o = 0
+    for i in range(len(options)):
+        if options[i] =="Wyjście":
+            print(f'[0] {options[i]}')
+        else:
+            o+=1
+            print(f'[{o}] {options[i]}')
+        
+def Error(fun):
+    print(f'Wystąpił błąd podczas wywoływania funkcji: {fun}')
